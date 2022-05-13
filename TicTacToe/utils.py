07cdcc5271a6402e9fff556c5a4711a6b9695340
Maik3000 <103969414+Maik3000@users.noreply.github.com>
2022-05-12 18:20:48 -0600

    correction

--- TicTacToe/utils.py
+++ TicTacToe/utils.py
@@ -116,8 +116,9 @@ def decide_move(board: list, player_id: str) -> [int, int]:
     
     else:
     ''' 
-    if board[0,0] and board[0,1] and board[0,2] and board[1,0] and board[1,1] and board[1,2] and board[2,0] and board[2,1] and board[2,2] == "-":
-        row, column = board[1,1]
+    if board[0][0] and board[0][1] and board[0][2] and board[1][0] and board[1][1] and board[1][2] and board[2][0] and board[2][1] and board[2][2] == "-":
+        row = [1]
+        column = [1]    
     
     return [row, column]
 
